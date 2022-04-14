6326487ff979495baf14da6375a471222de7dd8a
Marcos G.  J. de Jesus <marcosgjdj@gmail.com>
2022-04-13 23:30:12 -0300

    Confirmar

---
+++ SistemaWeb/Models/Classificacao.cs
@@ -0,0 +1,8 @@
+﻿namespace SistemaWeb.Models
+{
+    public class Classificacao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ SistemaWeb/Models/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaWeb.Models
+{
+    public class Conta
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        private string Descricao { get; set; }
+        [Required]
+        private DateTime DataPagamento { get; set; }
+
+        [Required]
+        private Decimal Valor { get; set; }
+        
+        private DateTime DataVencimento { get; set; }
+
+        public virtual Classificacao Classificacao { get; set; }
+        
+        public virtual Tipo Tipo { get; set; }
+    }
+}
